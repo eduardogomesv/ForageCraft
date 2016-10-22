34701b470de8d9aced05a7ce88e61628d2e629f7
theishiopian <theishiopian@gmail.com>
2016-10-21 22:27:05 -0700

    Incremented version number. Release imminent.

--- src/main/java/com/theishiopian/foragecraft/Reference.java
+++ src/main/java/com/theishiopian/foragecraft/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String MODID = "foragecraft";
     public static final String NAME = "ForageCraft";
-    public static final String VERSION = "1.7";
+    public static final String VERSION = "1.8";
 
     public static final String CLIENTPROXY = "com.theishiopian.foragecraft.proxy.Client";
     public static final String SERVERPROXY = "com.theishiopian.foragecraft.proxy.Server";
