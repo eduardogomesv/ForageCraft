ce7056d7ba8ad055d4aba9cbbc732502a73479e9
theishiopian <theishiopian@gmail.com>
2016-10-23 15:47:47 -0700

    Fixed versioning mismatch.

--- src/main/java/com/theishiopian/foragecraft/Reference.java
+++ src/main/java/com/theishiopian/foragecraft/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String MODID = "foragecraft";
     public static final String NAME = "ForageCraft";
-    public static final String VERSION = "1.8.1";
+    public static final String VERSION = "1.9";
     public static final String MC_VERSIONS = "[1.9,]";
     public static final String UPDATE_JSON = "https://raw.githubusercontent.com/theishiopian/ForageCraft/master/update.json";
 
