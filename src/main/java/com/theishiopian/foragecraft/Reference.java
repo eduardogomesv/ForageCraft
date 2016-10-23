046496ed35cab1d2745c6f492da3b00026a47c6c
Jonathan <jonathan@yolouso.com>
2016-10-23 15:14:10 -0400

    Added acceptedMinecraftVersions and updateJSON to @Mod

--- src/main/java/com/theishiopian/foragecraft/Reference.java
+++ src/main/java/com/theishiopian/foragecraft/Reference.java
@@ -5,6 +5,8 @@ public class Reference
     public static final String MODID = "foragecraft";
     public static final String NAME = "ForageCraft";
     public static final String VERSION = "1.8";
+    public static final String MC_VERSIONS = "[1.9,]";
+    public static final String UPDATE_JSON = "https://raw.githubusercontent.com/theishiopian/ForageCraft/master/update.json";
 
     public static final String CLIENTPROXY = "com.theishiopian.foragecraft.proxy.Client";
     public static final String SERVERPROXY = "com.theishiopian.foragecraft.proxy.Server";
