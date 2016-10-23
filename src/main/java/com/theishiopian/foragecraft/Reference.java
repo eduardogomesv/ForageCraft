5653b728471e51f8cc640370558b9726598845d8
theishiopian <theishiopian@gmail.com>
2016-10-23 16:02:56 -0700

    Fixed multi versioning

--- src/main/java/com/theishiopian/foragecraft/Reference.java
+++ src/main/java/com/theishiopian/foragecraft/Reference.java
@@ -5,7 +5,7 @@ public class Reference
     public static final String MODID = "foragecraft";
     public static final String NAME = "ForageCraft";
     public static final String VERSION = "1.9";
-    public static final String MC_VERSIONS = "[1.9,]";
+    public static final String MC_VERSIONS = "[1.9],[1.9.4],[1.10],[1.10.2]";
     public static final String UPDATE_JSON = "https://raw.githubusercontent.com/theishiopian/ForageCraft/master/update.json";
 
     public static final String CLIENTPROXY = "com.theishiopian.foragecraft.proxy.Client";
