f353a90e502916d86965825af3d613285bda0fb0
Jonathan <sansistypinggit@gmail.com>
2016-11-16 16:47:24 -0500

    Created JEI plugin class

---
+++ src/main/java/com/theishiopian/foragecraft/ForageCraftJEIPlugin.java
@@ -0,0 +1,14 @@
+package com.theishiopian.foragecraft;
+
+import javax.annotation.Nonnull;
+
+import mezz.jei.api.*;
+
+@JEIPlugin
+public class ForageCraftJEIPlugin extends BlankModPlugin
+{
+    public void register(@Nonnull IModRegistry registry)
+    {
+        IJeiHelpers jeiHelpers = registry.getJeiHelpers();
+    }
+}
