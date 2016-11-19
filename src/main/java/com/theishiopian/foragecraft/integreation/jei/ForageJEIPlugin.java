803b2ed045e28808286eac706d8521aa89a8b3dc
Jonathan <sansistypinggit@gmail.com>
2016-11-18 20:56:59 -0500

    Finished JEI descriptions for forage-able items. 1.10 release coming soon...

--- src/main/java/com/theishiopian/foragecraft/integreation/jei/ForageJEIPlugin.java
+++ src/main/java/com/theishiopian/foragecraft/integreation/jei/ForageJEIPlugin.java
@@ -20,6 +20,17 @@ public class ForageJEIPlugin extends BlankModPlugin
         IJeiHelpers jeiHelpers = registry.getJeiHelpers();
 
         registry.addDescription(new ItemStack(Items.STICK), "jei.description.foraging.stick");
+        registry.addDescription(new ItemStack(Items.CARROT), "jei.description.foraging.carrot");
+        registry.addDescription(new ItemStack(Items.POTATO), "jei.description.foraging.potato");
+        registry.addDescription(new ItemStack(Items.POISONOUS_POTATO), "jei.description.foraging.toxicpotato");
+        registry.addDescription(new ItemStack(Items.BEETROOT), "jei.description.foraging.beetroot");
+        registry.addDescription(new ItemStack(Items.BONE), "jei.description.foraging.bone");
+        registry.addDescription(new ItemStack(Items.SKULL), "jei.description.foraging.skulls");
+        registry.addDescription(new ItemStack(Items.FLINT), "jei.description.foraging.flint");
+        registry.addDescription(new ItemStack(Items.GOLD_NUGGET), "jei.description.foraging.goldnugget");
+        registry.addDescription(new ItemStack(Items.DIAMOND), "jei.description.foraging.diamond");
+        registry.addDescription(new ItemStack(Items.EMERALD), "jei.description.foraging.emerald");
+
     }
 
     @Override
