59235883359bb747bcc50e846e25324afab6a818
Jonathan <sansistypinggit@gmail.com>
2016-12-04 20:55:36 -0500

    Changed updateURL to the update.json on my webserver (sansistyping.me)

--- src/main/java/com/theishiopian/foragecraft/Reference.java
+++ src/main/java/com/theishiopian/foragecraft/Reference.java
@@ -6,7 +6,7 @@ public class Reference
     public static final String NAME = "ForageCraft";
     public static final String VERSION = "1.10";
     public static final String MC_VERSIONS = "[1.9],[1.9.4],[1.10],[1.10.2]";
-    public static final String UPDATE_JSON = "https://raw.githubusercontent.com/theishiopian/ForageCraft/master/update.json";
+    public static final String UPDATE_JSON = "https://sansistyping.me/foragecraft/update.json";
 
     public static final String CLIENTPROXY = "com.theishiopian.foragecraft.proxy.Client";
     public static final String SERVERPROXY = "com.theishiopian.foragecraft.proxy.Server";
