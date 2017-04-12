babbd92bb4e579e114961c6fa057f014a0dd9f22
Jonathan <sansistypinggit@gmail.com>
2017-04-12 18:43:36 -0400

    Fix: Mod updates would not synchronize with Forge.

--- src/main/java/com/theishiopian/foragecraft/Reference.java
+++ src/main/java/com/theishiopian/foragecraft/Reference.java
@@ -6,7 +6,7 @@ public class Reference
     public static final String NAME = "ForageCraft";
     public static final String VERSION = "1.11";
     public static final String MC_VERSIONS = "[1.11.2]";
-    public static final String UPDATE_JSON = "https://sansistyping.me/foragecraft/update.json";
+    public static final String UPDATE_JSON = "https://raw.githubusercontent.com/theishiopian/ForageCraft/master/update.json";
     public static final String CLIENTPROXY = "com.theishiopian.foragecraft.proxy.Client";
     public static final String SERVERPROXY = "com.theishiopian.foragecraft.proxy.Server";
 }
