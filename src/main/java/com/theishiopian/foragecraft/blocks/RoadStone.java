1a6535f41810f56db36b93212457cb6afc3982d1
theishiopian <theishiopian@gmail.com>
2017-06-30 13:46:44 -0700

    Road Stone now increases walking speed by %50

--- src/main/java/com/theishiopian/foragecraft/blocks/RoadStone.java
+++ src/main/java/com/theishiopian/foragecraft/blocks/RoadStone.java
@@ -5,6 +5,9 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class RoadStone extends Block
 {
@@ -17,4 +20,11 @@ public class RoadStone extends Block
 		setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
 		setSoundType(SoundType.STONE);
 	}
+	
+	 public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn)
+    {
+        super.onEntityWalk(worldIn, pos, entityIn);
+        
+        entityIn.setVelocity(entityIn.motionX*=1.5, 0, entityIn.motionZ*=1.5);
+    }
 }
