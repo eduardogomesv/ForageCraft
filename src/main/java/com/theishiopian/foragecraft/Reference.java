3ae6f10c492e10bbf5e4c7d3daee753be232434f
theishiopian <theishiopian@gmail.com>
2017-07-06 22:47:55 -0700

    Bumped version numbers in preperation for a new beta release.

--- src/main/java/com/theishiopian/foragecraft/Reference.java
+++ src/main/java/com/theishiopian/foragecraft/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String MODID = "foragecraft";
     public static final String NAME = "ForageCraft";
-    public static final String VERSION = "1.12.1";
+    public static final String VERSION = "1.12.2";
     public static final String MC_VERSIONS = "[1.11.2]";
     public static final String UPDATE_JSON = "https://raw.githubusercontent.com/theishiopian/ForageCraft/master/update.json";
     public static final String CLIENTPROXY = "com.theishiopian.foragecraft.proxy.Client";
