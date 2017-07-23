a09f12a3bd0217306970b09f32a382d9d71569f0
Jonathan <sansistypinggit@gmail.com>
2017-07-23 19:52:35 -0400

    Updated the IntelliJ Runs batch file

--- setup/make_intellij_runs.bat
+++ setup/make_intellij_runs.bat
@@ -1,8 +1,4 @@
 @echo off
-title Waiting...
-echo Before continuing, make sure IntelliJ IDEA is open on your workspace, and do not close it until after the batch script has finished.
-pause
-echo.
 title Generating run files for IntelliJ IDEA...
 echo Generating run files for IntelliJ IDEA...
 echo.
@@ -10,7 +6,7 @@ cd ..
 call gradlew.bat genIntellijRuns
 cd setup
 echo.
-echo Now that the runs have been generated, go back to IntelliJ IDEA. If a pop-up asks you to refresh the workspace, please do so.
-echo Make sure to edit configurations so the run file is set to have _main in it.
+echo Now that the runs have been generated, go back to IntelliJ IDEA.
+echo If a pop-up asks you to refresh the workspace, please do so.
 pause
 exit /b
