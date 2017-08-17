dddbea2ab62eaaffb98d70aff701c2cfb42afdb6
Jonathan <sansistypinggit@gmail.com>
2017-08-17 13:47:24 -0400

    Made things easier to read and find
    Theishiopian may disagree

---
+++ src/main/java/com/theishiopian/foragecraft/Descriptions.java
@@ -0,0 +1,44 @@
+package com.theishiopian.foragecraft;
+
+public class Descriptions
+{
+    /* ConfigVar */
+    // ConfigVar
+    public static final String configBranchChance = "The chance of a stick droping fom leaves";
+    public static final String configBranchMaxAmount = "The maximum number of sticks you can get from leaves";
+    public static final String configRootChance = "The chance of digging up a root (stick) from a grass block";
+    public static final String configRootMaxAmount = "The maximum amount of roots (sticks) you can get from a grass block";
+    public static final String configRockChance = "The chance of digging up a rock";
+    public static final String configRockMaxAmount = "The maximum amount of rocks you can get from various blocks";
+    public static final String configWildCarrotChance = "The chance of digging up a carrot from a grass block";
+    public static final String configWildCarrotMaxAmount = "The maximum amount of carrots you can get from a grass block";
+    public static final String configWildPotatoChance = "The chance of digging up a potato from a grass block";
+    public static final String configWildPotatoMaxAmount = "The maximum amount of potatoes you can get from a grass block";
+    public static final String configWildToxicPotatoChance = "The chance of digging up a poisonous potato from a grass block";
+    public static final String configWildToxicPotatoMaxAmount = "The maximum amount of poisonous potatoes you can get from a grass block";
+    public static final String configWildBeetRootChance = "The chance of digging up a beet root from a grass block";
+    public static final String configWildBeetRootMaxAmount = "The maximum amount of beet root you can get from a grass block";
+    public static final String configBuriedBonesChance = "The chance of uncovering ancient bones";
+    public static final String configBuriedBonesMaxBoneAmount = "The maximum amount of bones you can get from buried skeletons";
+    public static final String configBuriedBonesMaxSkullAmount = "The maximum amount of skulls buried skeletons can have";
+    public static final String configDeepRootChance = "The chance of digging up a stick from a dirt block";
+    public static final String configDeepRootMaxAmount = "The maximum amount of roots (sticks) you can get from a dirt block";
+    public static final String configBuriedFlintChance = "The chance of finding flint in a dirt block";
+    public static final String configBuriedFlintMaxAmount = "The maximum amount of flint you can get from a dirt block";
+    public static final String configGoldChance = "The chance of finding gold (in nugget form) inside a stone block";
+    public static final String configGoldMaxAmount = "The maximum amount of gold nuggets you can find in a stone block";
+    public static final String configFlintChipChance = "The chance of finding flint in a stone block";
+    public static final String configFlintChipMaxAmount = "The maximum amount of flint you gant get from a stone block";
+    public static final String configCoalDiamondChance = "The chance of finding diamond while mining coal";
+    public static final String configCoalDiamondMaxAmount = "The maximum amount of diamonds you can get from coal ore";
+    public static final String configCoalEmeraldChance = "The chance of finding emerald while mining coal";
+    public static final String configCoalEmeraldMaxAmount = "The maximum amount of emeralds you can get from coal ore";
+    public static final String configNetherGoldChance = "The chance of getting gold (in nugget form) from nether quartz";
+    public static final String configNetherGoldMaxAmount = "The maximum amount of gold nuggets you can get from nether quartz";
+    public static final String configPumpkinSeeds = "This determines whether or not you can find pumpkin seeds in grass";
+    public static final String configMelonSeeds = "This determines whether or not you can find melon seeds in grass";
+    public static final String configBeetrootSeeds = "This determines whether or not you can find beetroot seeds in grass";
+    public static final String configJeiNoInt = "Set this to true if you want to disable JEI descriptions for vanilla items";
+    public static final String configDisableSticks = "Set this to true if you want to disable sticks generation";
+    public static final String configDisableRocks = "Set this to true if you want to disable rocks generation";
+}
--- src/main/java/com/theishiopian/foragecraft/ForageCraftMod.java
+++ src/main/java/com/theishiopian/foragecraft/ForageCraftMod.java
@@ -12,7 +12,6 @@ import com.theishiopian.foragecraft.proxy.CommonProxy;
 import com.theishiopian.foragecraft.world.generation.FCMasterWorldGenerator;
 
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -21,6 +20,9 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+
+//Unused imports?
+import net.minecraftforge.common.config.Config;
 
 @Mod(modid=Reference.MODID,name=Reference.NAME,version=Reference.VERSION,acceptedMinecraftVersions=Reference.MC_VERSIONS,updateJSON=Reference.UPDATE_JSON)
 
@@ -42,69 +44,69 @@ public class ForageCraftMod
 		config.load();
 
 		//leaves
-		ConfigVariables.branchChance = config.getFloat("Branch chance", "Sticks from leaves", 1.0f, 0.0f, 1.0f, "The chance of a stick droping fom leaves");
-		ConfigVariables.branchMaxAmount = config.getInt("Maximum branch amount", "Sticks from leaves", 1, 0, 64, "The maximum number of sticks you can get from leaves");
+		ConfigVariables.branchChance = config.getFloat("Branch chance", "Sticks from leaves", 1.0f, 0.0f, 1.0f, Descriptions.configBranchChance);
+		ConfigVariables.branchMaxAmount = config.getInt("Maximum branch amount", "Sticks from leaves", 1, 0, 64, Descriptions.configBranchMaxAmount);
 		
 		//grass block
-		ConfigVariables.rootChance = config.getFloat("Root chance", "Sticks from grass blocks", 0.09f, 0.0f, 1.0f, "The chance of digging up a root (stick) from a grass block");
-		ConfigVariables.rootMaxAmount=config.getInt("Maximum root amount", "Sticks from grass blocks", 1, 0, 64, "The maximum amount of roots (sticks) you can get from a grass block");
+		ConfigVariables.rootChance = config.getFloat("Root chance", "Sticks from grass blocks", 0.09f, 0.0f, 1.0f, Descriptions.configRootChance);
+		ConfigVariables.rootMaxAmount=config.getInt("Maximum root amount", "Sticks from grass blocks", 1, 0, 64, Descriptions.configRootMaxAmount);
 		
-		ConfigVariables.rockChance = config.getFloat("Rock chance", "Rocks from various blocks", 0.09f, 0.0f, 1.0f, "The chance of digging up a rock");
-		ConfigVariables.rockMaxAmount = config.getInt("Maximum rock amount", "Rocks from various blocks", 1, 0, 64, "The maximum amount of rocks you can get from various blocks");
+		ConfigVariables.rockChance = config.getFloat("Rock chance", "Rocks from various blocks", 0.09f, 0.0f, 1.0f, Descriptions.configRockChance);
+		ConfigVariables.rockMaxAmount = config.getInt("Maximum rock amount", "Rocks from various blocks", 1, 0, 64, Descriptions.configRockMaxAmount);
 		
-		ConfigVariables.wildCarrotChance = config.getFloat("Carrot chance", "Carrots from grass blocks", 0.01f, 0.0f, 1.0f, "The chance of digging up a carrot from a grass block");
-		ConfigVariables.wildCarrotMaxAmount = config.getInt("Maximum carrot amount", "Carrots from grass blocks", 1, 0, 64, "The maximum amount of carrots you can get from a grass block");
+		ConfigVariables.wildCarrotChance = config.getFloat("Carrot chance", "Carrots from grass blocks", 0.01f, 0.0f, 1.0f, Descriptions.configWildCarrotChance);
+		ConfigVariables.wildCarrotMaxAmount = config.getInt("Maximum carrot amount", "Carrots from grass blocks", 1, 0, 64, Descriptions.configWildCarrotMaxAmount);
 		
-		ConfigVariables.wildPotatoChance = config.getFloat("Potato chance", "Potatoes from grass blocks", 0.01f, 0.0f, 1.0f, "The chance of digging up a potato from a grass block");
-		ConfigVariables.wildPotatoMaxAmount = config.getInt("Maximum potato amount", "Potatoes from grass blocks", 1, 0, 64, "The maximum amount of potatoes you can get from a grass block");
+		ConfigVariables.wildPotatoChance = config.getFloat("Potato chance", "Potatoes from grass blocks", 0.01f, 0.0f, 1.0f, Descriptions.configWildPotatoChance);
+		ConfigVariables.wildPotatoMaxAmount = config.getInt("Maximum potato amount", "Potatoes from grass blocks", 1, 0, 64, Descriptions.configWildPotatoMaxAmount);
 		
-		ConfigVariables.wildToxicPotatoChance = config.getFloat("Poisonous potato chance", "Potatoes from grass blocks", 0.01f, 0.0f, 1.0f, "The chance of digging up a poisonous potato from a grass block");
-		ConfigVariables.wildToxicPotatoMaxAmount = config.getInt("Maximum poisonous potato amount", "Potatoes from grass blocks", 1, 0, 64, "The maximum amount of poisonous potatoes you can get from a grass block");
+		ConfigVariables.wildToxicPotatoChance = config.getFloat("Poisonous potato chance", "Potatoes from grass blocks", 0.01f, 0.0f, 1.0f, Descriptions.configWildToxicPotatoChance);
+		ConfigVariables.wildToxicPotatoMaxAmount = config.getInt("Maximum poisonous potato amount", "Potatoes from grass blocks", 1, 0, 64, Descriptions.configWildToxicPotatoMaxAmount);
 		
-		ConfigVariables.wildBeetRootChance = config.getFloat("Beet root chance", "Beet roots from grass blocks", 0.01f, 0.0f, 1.0f, "The chance of digging up a beet root from a grass block");
-		ConfigVariables.wildBeetRootMaxAmount = config.getInt("Maximum beet root amount", "Beet roots from grass blocks", 1, 0, 64, "The maximum amount of beet root you can get from a grass block");
+		ConfigVariables.wildBeetRootChance = config.getFloat("Beet root chance", "Beet roots from grass blocks", 0.01f, 0.0f, 1.0f, Descriptions.configWildBeetRootChance);
+		ConfigVariables.wildBeetRootMaxAmount = config.getInt("Maximum beet root amount", "Beet roots from grass blocks", 1, 0, 64, Descriptions.configWildBeetRootMaxAmount);
 		
 		//bones
-		ConfigVariables.buriedBonesChance = config.getFloat("Buried bones chance", "Buried Bones", 0.005f, 0.0f, 1.0f, "The chance of uncovering ancient bones");
-		ConfigVariables.buriedBonesMaxBoneAmount = config.getInt("Maximum buried bones amount", "Buried Bones", 9, 0, 64, "The maximum amount of bones you can get from buried skeletons");
-		ConfigVariables.buriedBonesMaxSkullAmount = config.getInt("Maximum buried skull amount", "Buried Bones", 1, 0, 64, "The maximum amount of skulls buried skeletons can have");
+		ConfigVariables.buriedBonesChance = config.getFloat("Buried bones chance", "Buried Bones", 0.005f, 0.0f, 1.0f, Descriptions.configBuriedBonesChance);
+		ConfigVariables.buriedBonesMaxBoneAmount = config.getInt("Maximum buried bones amount", "Buried Bones", 9, 0, 64, Descriptions.configBuriedBonesMaxBoneAmount);
+		ConfigVariables.buriedBonesMaxSkullAmount = config.getInt("Maximum buried skull amount", "Buried Bones", 1, 0, 64, Descriptions.configBuriedBonesMaxSkullAmount);
 		
 		//dirt
-		ConfigVariables.deepRootChance = config.getFloat("Deep root chance", "Sticks from dirt blocks", 0.07f, 0.0f, 1.0f, "The chance of digging up a stick from a dirt block");
-		ConfigVariables.deepRootMaxAmount = config.getInt("Maximum deep root amount", "Sticks from dirt blocks", 1, 0, 64, "The maximum amount of roots (sticks) you can get from a dirt block");
+		ConfigVariables.deepRootChance = config.getFloat("Deep root chance", "Sticks from dirt blocks", 0.07f, 0.0f, 1.0f, Descriptions.configDeepRootChance);
+		ConfigVariables.deepRootMaxAmount = config.getInt("Maximum deep root amount", "Sticks from dirt blocks", 1, 0, 64, Descriptions.configDeepRootMaxAmount);
 		
-		ConfigVariables.buriedFlintChance = config.getFloat("Buried Flint Chance", "Buried Flint", 0.04f, 0.0f, 1.0f, "The chance of finding flint in a dirt block");
-		ConfigVariables.buriedFlintMaxAmount = config.getInt("Maximum buried flint amount", "Buried Flint", 1, 0, 64, "The maximum amount of flint you can get from a dirt block");
+		ConfigVariables.buriedFlintChance = config.getFloat("Buried Flint Chance", "Buried Flint", 0.04f, 0.0f, 1.0f, Descriptions.configBuriedFlintChance);
+		ConfigVariables.buriedFlintMaxAmount = config.getInt("Maximum buried flint amount", "Buried Flint", 1, 0, 64, Descriptions.configBuriedFlintMaxAmount);
 		
 		//stone
-		ConfigVariables.goldChance = config.getFloat("Gold chance", "Gold in stone", 0.005f, 0.0f, 1.0f, "The chance of finding gold (in nugget form) inside a stone block");
-		ConfigVariables.goldMaxAmount = config.getInt("Maximum gold amount", "Gold in stone", 1, 0, 64, "The maximum amount of gold nuggets you can find in a stone block");
+		ConfigVariables.goldChance = config.getFloat("Gold chance", "Gold in stone", 0.005f, 0.0f, 1.0f, Descriptions.configGoldChance);
+		ConfigVariables.goldMaxAmount = config.getInt("Maximum gold amount", "Gold in stone", 1, 0, 64, Descriptions.configGoldMaxAmount);
 		
-		ConfigVariables.flintChipChance = config.getFloat("Flint chip chance", "Flint chips", 0.05f, 0.0f, 1.0f, "The chance of finding flint in a stone block");
-		ConfigVariables.flintChipMaxAmount = config.getInt("Maximum flint chip amount", "Flint chips", 1, 0, 64, "The maximum amount of flint you gant get from a stone block");
+		ConfigVariables.flintChipChance = config.getFloat("Flint chip chance", "Flint chips", 0.05f, 0.0f, 1.0f, Descriptions.configFlintChipChance);
+		ConfigVariables.flintChipMaxAmount = config.getInt("Maximum flint chip amount", "Flint chips", 1, 0, 64, Descriptions.configFlintChipMaxAmount);
 		
 		//coal ore
-		ConfigVariables.coalDiamondChance = config.getFloat("Coal diamond chance", "Coal gems", 0.001f, 0.0f, 1.0f, "The chance of finding diamond while mining coal");
-		ConfigVariables.coalDiamondMaxAmount = config.getInt("Maximum coal diamond amount", "Coal gems", 1, 0, 64, "The maximum amount of diamonds you can get from coal ore");
+		ConfigVariables.coalDiamondChance = config.getFloat("Coal diamond chance", "Coal gems", 0.001f, 0.0f, 1.0f, Descriptions.configCoalDiamondChance);
+		ConfigVariables.coalDiamondMaxAmount = config.getInt("Maximum coal diamond amount", "Coal gems", 1, 0, 64, Descriptions.configCoalDiamondMaxAmount);
 		
-		ConfigVariables.coalEmeraldChance = config.getFloat("Coal emerald chance", "Coal gems", 0.001f, 0.0f, 1.0f, "The chance of finding emerald while mining coal");
-		ConfigVariables.coalEmeraldMaxAmount = config.getInt("Maximum coal emerald amount", "Coal gems", 1, 0, 64, "The maximum amount of emeralds you can get from coal ore");
+		ConfigVariables.coalEmeraldChance = config.getFloat("Coal emerald chance", "Coal gems", 0.001f, 0.0f, 1.0f, Descriptions.configCoalEmeraldChance);
+		ConfigVariables.coalEmeraldMaxAmount = config.getInt("Maximum coal emerald amount", "Coal gems", 1, 0, 64, Descriptions.configCoalEmeraldMaxAmount);
 		
 		//nether quartz
-		ConfigVariables.netherGoldChance = config.getFloat("Nether gold chance", "Nether gold", 1.0f, 0.0f, 1.0f, "The chance of getting gold (in nugget form) from nether quartz");
-		ConfigVariables.netherGoldMaxAmount = config.getInt("Maximum nether gold amount", "Nether gold", 9, 0, 64, "The maximum amount of gold nuggets you can get from nether quartz");
+		ConfigVariables.netherGoldChance = config.getFloat("Nether gold chance", "Nether gold", 1.0f, 0.0f, 1.0f, Descriptions.configNetherGoldChance);
+		ConfigVariables.netherGoldMaxAmount = config.getInt("Maximum nether gold amount", "Nether gold", 9, 0, 64, Descriptions.configNetherGoldMaxAmount);
 		
 		//seeds from grass
-		ConfigVariables.pumpkinSeeds = config.getBoolean("Pumpkin seeds from grass", "Pumpkin seeds", true, "This determines whether or not you can find pumpkin seeds in grass");
-		ConfigVariables.melonSeeds = config.getBoolean("Melon seeds from grass", "Melon seeds", true, "This determines whether or not you can find melon seeds in grass");
-		ConfigVariables.beetrootSeeds = config.getBoolean("Beetroot seeds from grass", "Beetroot seeds", true, "This determines whether or not you can find beetroot seeds in grass");
+		ConfigVariables.pumpkinSeeds = config.getBoolean("Pumpkin seeds from grass", "Pumpkin seeds", true, Descriptions.configPumpkinSeeds);
+		ConfigVariables.melonSeeds = config.getBoolean("Melon seeds from grass", "Melon seeds", true, Descriptions.configMelonSeeds);
+		ConfigVariables.beetrootSeeds = config.getBoolean("Beetroot seeds from grass", "Beetroot seeds", true, Descriptions.configBeetrootSeeds);
 
 		//JEI Integration
-		ConfigVariables.jeiNoInt = config.getBoolean("Disable JEI Integration", "Mod Integration", false, "Set this to true if you want to disable JEI descriptions for vanilla items");
+		ConfigVariables.jeiNoInt = config.getBoolean("Disable JEI Integration", "Mod Integration", false, Descriptions.configJeiNoInt);
 
 		//Disable sticks or rocks
-		ConfigVariables.disableSticks = config.getBoolean("Disable sticks generation", "World Generation", false, "Set this to true if you want to disable sticks generation");
-		ConfigVariables.disableRocks = config.getBoolean("Disable rocks generation", "World Generation", false, "Set this to true if you want to disable rocks generation");
+		ConfigVariables.disableSticks = config.getBoolean("Disable sticks generation", "World Generation", false, Descriptions.configDisableSticks);
+		ConfigVariables.disableRocks = config.getBoolean("Disable rocks generation", "World Generation", false, Descriptions.configDisableRocks);
 
 		//Save config
 		config.save();
